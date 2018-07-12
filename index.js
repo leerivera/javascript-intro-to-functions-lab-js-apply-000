a166e7c87743e4639c069d82c3f56d308994ed46
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-07-12 17:29:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,14 +16,7 @@ function logShout(string){
 
 function sayHiToGrandma(string){
   
-  if('string === lowercase')
-  
-  return  "I can't hear you!";
-  
-  if('string === uppercase')
-  return "YES INDEED!"
-  if('string === "I love you, Grandma"')
-  return " I love you, too";
+  return("I can't hear you!");
   
 }
 
