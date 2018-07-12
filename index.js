867fc6215a8decfd1992c6085227d2068f770643
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-07-12 17:39:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string){
 function logShout(string){
   console.log(string.toUpperCase())
 }
-
+var string = "I can't hear you"
 function sayHiToGrandma(string){
   
   if('string === "I love you, Grandma')
